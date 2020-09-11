cb1f0db3de8cf805b97904b8ef16858a2749f938
Austin-Grewell <70531558+Austin-Grewell@users.noreply.github.com>
2020-09-11 17:27:57 -0600

    Added Create_Phone_Number

---
+++ Create Phone Number/Logic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Create_Phone_Number
+{
+    class Logic
+    {
+    }
+}
---
+++ Create Phone Number/UnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Create_Phone_Number
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void SomeTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void SomeOtherTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+
+    public static class UnitTestHelpers
+    {
+        //Helper libraries for this class
+    }
+}

02beda569d677b3200d1da241f93f2fa8792fad6
Austin-Grewell <70531558+Austin-Grewell@users.noreply.github.com>
2020-09-14 17:36:12 -0600

    Added to Creat Phone Number

--- Create Phone Number/Logic.cs
+++ Create Phone Number/Logic.cs
@@ -6,5 +6,46 @@ namespace Create_Phone_Number
 {
     class Logic
     {
+        internal static string TranslateToPhoneNumber(int[] numbers)
+        {
+
+            //Want (XXX) XXX-XXXX
+
+            //Queue<string> queue = new Queue<string>();
+
+            //queue.Enqueue("(");
+
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    queue.Enqueue(numbers[i].ToString());
+            //}
+
+            //queue.Enqueue(") ");
+
+            //for (int i = 3; i < 6; i++)
+            //{
+            //    queue.Enqueue(numbers[i].ToString());
+            //}
+
+            //queue.Enqueue("-");
+
+
+            //for (int i = 6; i < 10; i++)
+            //{
+            //    queue.Enqueue(numbers[i].ToString());
+            //}
+
+            //StringBuilder sb = new StringBuilder();
+
+            //while (queue.Count != 0)
+            //{
+            //    sb.Append(queue.Dequeue());
+            //}
+
+            //return sb.ToString();
+
+            return $"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[4]}{numbers[5]}-{numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+
+        }
     }
 }

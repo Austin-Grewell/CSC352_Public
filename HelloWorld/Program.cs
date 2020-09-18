fbde47e621451d1e1c4b754fadec5f311376367b
Austin-Grewell <70531558+Austin-Grewell@users.noreply.github.com>
2020-09-18 16:31:12 -0600

    Hello World

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            static bool IsEven(int i)
+            {
+                return i % 2 == 0;
+            }
+        }
+    }
+}

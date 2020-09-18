5c5a33226540f614e62a0e0920d476779dd7d5a7
Austin-Grewell <70531558+Austin-Grewell@users.noreply.github.com>
2020-09-18 17:34:00 -0600

    Did Summation and Factorial

--- Summation/Logic.cs
+++ Summation/Logic.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Summation
 {
@@ -6,7 +7,47 @@ namespace Summation
     {
         public static long Summation(long n)
         {
-            throw new NotSupportedException();
+
+            //Recursive
+            /*if (n == 0)
+            {
+                return 0;
+            } 
+            else
+            {
+                long nextNumber = Summation(n - 1);
+                return n + nextNumber;
+            }*/
+
+
+            //Iterative
+            /*long result = 0;
+
+            for (long i = n; i > 0; i--)
+            {
+                result = result + i;
+            }
+
+            return result;*/
+
+
+            //Stack
+            Stack<long> stack = new Stack<long>();
+
+            while (n != 0)
+            {
+                stack.Push(n);
+                n = n - 1;
+            }
+
+            long result = 0;
+
+            while (stack.Count != 0)
+            {
+                result = result + stack.Pop();
+            }
+
+            return result;
         }
     }
 }
